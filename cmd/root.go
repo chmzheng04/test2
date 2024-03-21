9c67249abcef6ee6e30442372914d77e3a190873
chmzheng04 <58098984+chmzheng04@users.noreply.github.com>
2024-03-21 16:54:40 +0800

    Revert "Update root.go"

--- cmd/root.go
+++ cmd/root.go
@@ -24,35 +24,9 @@ import (
 	"github.com/spf13/viper"
 	""
 )
-var rootCmd = &cobra.Command{
-	Use:   "newCLI",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
-}
-
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	cobra.CheckErr(rootCmd.Execute())
-}
 
 var cfgFile string
 var a = 0000;
-var p = "amaTeat"
-var q = "asss"
-var cfgFile string
-var a = 0000;
-var p = "amaTeat"
-var q = "asss"
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
